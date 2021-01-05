81d3ff301da556e2d0b0193f3118c616ae6f17f6
cmbim360 <77018523+cmbim360@users.noreply.github.com>
2021-01-05 10:32:56 -0800

    Update from plugin

--- plugin.js
+++ plugin.js
@@ -1,10 +1,19 @@
 const createBlock = async (w,l,h) => {
-    const pt1 = await WSM.Geom.Point3d(0,0,24);
+    const pt1 = await WSM.Geom.Point3d(0,0,0);
     const pt2 = await WSM.Geom.Point3d(w,l,h);
     const histID = await FormIt.GroupEdit.GetEditingHistoryID();
     console.log(histID, pt1, pt2)
 
     const test = await WSM.APICreateBlock(histID, pt1, pt2);
+}
+
+const createCylinder = async (w,l,h) => {
+    const pt1 = await WSM.Geom.Point3d(0,0,0);
+    const pt2 = await WSM.Geom.Point3d(w,l,h);
+    const histID = await FormIt.GroupEdit.GetEditingHistoryID();
+    console.log(histID, pt1, pt2)
+
+    const test = await WSM.APICreateCylinder(histID, pt1, w/2, h);
 }
 
 document.getElementById("CreateBlockBtn").addEventListener("click", () => {
@@ -13,4 +22,13 @@ document.getElementById("CreateBlockBtn").addEventListener("click", () => {
     const l = Number(document.getElementById("Length").value);
 
     createBlock(w,l,h);
+});
+
+
+document.getElementById("CreateCylinderBtn").addEventListener("click", () => {
+    const w = Number(document.getElementById("Width").value);
+    const h = Number(document.getElementById("Height").value);
+    const l = Number(document.getElementById("Length").value);
+
+    createCylinder(w,l,h);
 });
